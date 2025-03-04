a97b6624b55337aca98afc5be72f30fae320eac6
ananya-dot <ananyasachdev2005@gmail.com>
2025-03-04 17:26:19 +0530

    add a new note with input

--- app/lib/evalio_app_web/live/page_live.ex
+++ app/lib/evalio_app_web/live/page_live.ex
@@ -4,18 +4,48 @@ defmodule EvalioAppWeb.PageLive do
   # import PetalComponents
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :count, 0)}
+    {:ok, assign(socket, show_form: false, title: "", content: "")}
   end
+
+  # def mount(_params, _session, socket) do
+  #   {:ok, assign(socket, show_form: false, title: "", content: "")}
+  # end
 
   def render(assigns) do
     ~H"""
-    <.button color="success" label="Success" />
+    <div class="flex justify-start">
+      <.button color="primary" label="New Note" phx-click="toggle_form" />
+    </div>
+      <%= if @show_form do %>
+      <div class="mt-4 max-w-md">
+        <.form phx-submit="save_note">
+        <PetalComponents.Input.input name="title" label="Title" placeholder="Note Title" value={@title} />
+        <PetalComponents.Input.input name="content" label="Content" placeholder="Write your note here..." value={@content} />
+
+          <div class="mt-4">
+            <.button label="Save" color="green" />
+            <.button label="Cancel" color="red" phx-click="toggle_form" type="button" />
+          </div>
+        </.form>
+      </div>
+    <% end %>
     """
   end
 
-  # def handle_event("clicked", _params, socket) do
-  #   IO.puts("Button clicked!")
-  #   {:noreply, socket}
-  # end
 
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, show_form: false, title: "", content: "")}
+  end
+
+
+  def handle_event("toggle_form", _params, socket) do
+    IO.inspect("Toggle form clicked")
+    {:noreply, assign(socket, show_form: !socket.assigns.show_form)}
+  end
+
+  def handle_event("save_note", %{"title" => title, "content" => content}, socket) do
+    IO.puts("New Note: #{title} - #{content}")
+    {:noreply, assign(socket, show_form: false, title: "", content: "")}
+  end
+  
 end
