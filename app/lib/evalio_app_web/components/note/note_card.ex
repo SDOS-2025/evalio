eb36c95a7299633084fa8069df10b2413898b17d
Nishchaya Roy <nishchayaroy17@gmail.com>
2025-03-28 15:05:24 +0530

    update note card, add tag menu(UI only)

--- app/lib/evalio_app_web/components/note/note_card.ex
+++ app/lib/evalio_app_web/components/note/note_card.ex
@@ -1,7 +1,7 @@
 defmodule EvalioAppWeb.NoteCard do
   use EvalioAppWeb, :live_component
   import PetalComponents
-
+  alias EvalioAppWeb.NoteTagMenu
   def render(assigns) do
     ~H"""
     <div>
@@ -9,23 +9,8 @@ defmodule EvalioAppWeb.NoteCard do
         <div class="flex justify-between items-center">
           <h3 class="font-bold text-lg"><%= @note.title %></h3>
           <div class="flex items-center space-x-2">
-            <!-- Orange Button with Dropdown -->
-            <div class="relative">
-              <.dropdown>
-                <:trigger_element>
-                  <div class="w-[60px] h-[30px] bg-[#FF655F] text-white text-sm flex items-center justify-center rounded-full">
-                    <!-- Toggle Button (if needed) -->
-                  </div>
-                </:trigger_element>
-                <.dropdown_menu_item phx-click="change_color" phx-value-color="#FF655F" label="Red" />
-                <.dropdown_menu_item phx-click="change_color" phx-value-color="#FFA500" label="Orange" />
-                <.dropdown_menu_item phx-click="change_color" phx-value-color="#FFFF00" label="Yellow" />
-                <.dropdown_menu_item phx-click="change_color" phx-value-color="#008000" label="Green" />
-                <.dropdown_menu_item phx-click="change_color" phx-value-color="#0000FF" label="Blue" />
-                <.dropdown_menu_item phx-click="change_color" phx-value-color="#800080" label="Purple" />
-              </.dropdown>
-            </div>
 
+          <.live_component module={NoteTagMenu} id={"note-tag-menu-#{@note.id}"} note={@note} />
             <!-- Delete Button -->
             <button phx-click="delete_note" phx-value-index={@index} phx-target={@myself} class="text-red-500">
               <HeroiconsV1.Outline.trash class="w-5 h-5 cursor-pointer" />
