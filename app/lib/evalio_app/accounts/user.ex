b9908adade4ab9b60bc22e4535502a8911ce3379
ananya-dot <ananyasachdev2005@gmail.com>
2025-04-12 14:10:04 +0530

    add login/signup page

---
+++ app/lib/evalio_app/accounts/user.ex
@@ -0,0 +1,30 @@
+defmodule EvalioApp.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Bcrypt.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
--- app/mix.exs
+++ app/mix.exs
@@ -58,7 +58,8 @@ defmodule EvalioApp.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:petal_components, "~> 2.8.2"}
+      {:petal_components, "~> 2.8.2"},
+      {:bcrypt_elixir, "~> 3.0"}
     ]
   end
 
