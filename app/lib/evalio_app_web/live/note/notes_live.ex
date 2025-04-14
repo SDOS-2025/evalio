4363a7519511a3372c4fb3f099eb2a30468d6d4b
ananya-dot <ananyasachdev2005@gmail.com>
2025-04-14 18:58:51 +0530

    add mentees, mentors and cohort pages

--- app/lib/evalio_app_web/live/note/notes_live.ex
+++ app/lib/evalio_app_web/live/note/notes_live.ex
@@ -178,6 +178,11 @@ defmodule EvalioAppWeb.NotesLive do
   end
 
   @impl true
+  def handle_event("navigate", %{"to" => path}, socket) do
+    {:noreply, push_navigate(socket, to: path)}
+  end
+
+  @impl true
   def handle_event("logout", _params, socket) do
     {:noreply, push_navigate(socket, to: ~p"/login")}
   end
