1c64bf4cb2b2ca9492f0db653057ec24632507fd
ananya-garg-04 <garg.ananya2018@gmail.com>
2025-05-05 00:53:33 +0530

    changes in sign in page ui

--- app/lib/evalio_app_web/live/auth_live.ex
+++ app/lib/evalio_app_web/live/auth_live.ex
@@ -105,10 +105,20 @@ defmodule EvalioAppWeb.AuthLive do
             <div>
               <button
                 type="submit"
-                class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 {if @page == "login", do: "Sign in", else: "Sign up"}
               </button>
+            </div>
+
+            <div class="mt-6">
+              <a
+                href="http://127.0.0.1:5000/login/google"
+                class="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+              >
+                <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" alt="Google Logo" class="h-5 mr-2" />
+                Sign in with Google
+              </a>
             </div>
           </.form>
 
@@ -128,20 +138,11 @@ defmodule EvalioAppWeb.AuthLive do
               <button
                 phx-click="switch-page"
                 phx-value-page={if @page == "login", do: "signup", else: "login"}
-                class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-blue-600 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 {if @page == "login", do: "Create an account", else: "Sign in"}
               </button>
             </div>
-          </div>
-
-          <div class="mt-6">
-            <a
-              href="http://127.0.0.1:5000/login/google"
-              class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-            >
-              Sign in with Google
-            </a>
           </div>
         </div>
       </div>
